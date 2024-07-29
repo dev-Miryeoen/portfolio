f76a9321aec98a16e2de475a8c4f64db107d2a9d
sangwon <derange63@gmail.com>
2024-07-29 20:17:28 +0900

    change footer

--- src/page/Main.js
+++ src/page/Main.js
@@ -22,19 +22,14 @@ const Main = () => {
                 <footer className="footer bg-dark">
                 <div className="container">
                     <div className="row">
-                    <div className="col-sm-6">
-                        <p className="copyright font-alt">&copy; 2017&nbsp;<a href="index.html">TitaN</a>, All Rights Reserved</p>
-                    </div>
-                    <div className="col-sm-6">
-                        <div className="footer-social-links"><a href="#"><i className="fa fa-facebook"></i></a><a href="#"><i className="fa fa-twitter"></i></a><a href="#"><i className="fa fa-dribbble"></i></a><a href="#"><i className="fa fa-skype"></i></a>
-                        </div>
+                    <div className="col-sm-12">
+                        <p className="copyright font-alt">&copy; 2024&nbsp;<a href="/">sw portfolio</a></p>
                     </div>
                     </div>
                 </div>
                 </footer>
             </div>
             <div className="scroll-up"><a href="#totop"><i className="fa fa-angle-double-up"></i></a></div>
-            <script async="" defer="" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDK2Axt8xiFYMBMDwwG1XzBQvEbYpzCvFU"></script>
         </main>
     );
 };
